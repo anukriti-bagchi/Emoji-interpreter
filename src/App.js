2ab1c419154f5bac9d649bab739e409c768af26a
Anukriti Bagchi <91126891+anukriti-bagchi@users.noreply.github.com>
2021-10-10 22:46:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😂": "face with tears of joy",
+  "😢": "sad",
+  "😉": "winking face",
+  "❤️": "love",
+  "😐": "neutral face"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function emojiInputHandler(event) {
+    //processing
+    var userInput = event.target.value;
+
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+
+    setMeaning(meaning); //react call to show output
+  }
+
+  function emojiClickHandler(emoji) {
+    //processing
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning); //react call to show output
+  }
+
+  return (
+    <div className="App">
+      <h1> inside out!!</h1>
+      <input onChange={emojiInputHandler} />
+      <h2> {meaning} </h2>
+      {/* actual output set by react using usestate  */}
+
+      <h3> emojis we know </h3>
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}{" "}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+/**
+ * 
+ if we do the above thing using prompt it could be like this:
+ var username = prompt("Give me your name");
+ */
+
+/**
+ * things to notice,
+ * in react in place of class we use className
+ * style takes an object instead of a string in jsx
+ * tags and attributes works similarly as in html
+ */
